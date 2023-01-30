2da660569823430fc482cfe7227de2b3a4deb09c
Pedro Batista <pedro@yellowberry.com.br>
2023-01-30 08:48:32 -0300

    created and styled the search form to find any transaction.

--- src/Pages/Transactions/index.tsx
+++ src/Pages/Transactions/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summay";
+import { SearchForm } from "./components/SearchForm";
 import { PriceHighLight, TransactionContainer, TransactionTable } from "./styles";
 
 export function Transactions() {
@@ -9,6 +10,9 @@ export function Transactions() {
       <Summary/>
     
       <TransactionContainer>
+        
+        <SearchForm/>
+
         <TransactionTable>
           <tbody>
             <tr>
