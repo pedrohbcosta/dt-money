3c4205f6fe33d2250b7008fe380ba2516475cd2e
Pedro Batista <pedro@yellowberry.com.br>
2023-01-30 07:58:32 -0300

    created the summary section to show the current balance of account and started the stylization.

--- src/Pages/Transactions/index.tsx
+++ src/Pages/Transactions/index.tsx
@@ -1,10 +1,12 @@
 import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summay";
 import { TransactionContainer } from "./styles";
 
 export function Transactions() {
   return (
     <TransactionContainer>
       <Header/>
+      <Summary/>
     </TransactionContainer>
   )
 }
