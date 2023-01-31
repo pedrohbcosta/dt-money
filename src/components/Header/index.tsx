60ad2731a0069b52cf9d0d6553dc73f5cb5a1ecb
Pedro Batista <pedro@yellowberry.com.br>
2023-01-31 08:36:23 -0300

    To keep the code clean, we created a new components to customize the portal on the modal.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,7 @@ import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import * as Dialog from "@radix-ui/react-dialog"
 
 import dtLogo from "../../assets/dtLogo.svg";
+import { NewTransactionModal } from "../NewTransactionModal";
 
 export function Header() {
   return (
@@ -10,19 +11,12 @@ export function Header() {
         <img src={ dtLogo } alt="" />
         
         <Dialog.Root>
+          
           <Dialog.Trigger asChild>
             <NewTransactionButton>Nova transação</NewTransactionButton>
           </Dialog.Trigger>
-          
-          <Dialog.Portal>
-            <Dialog.Overlay />
 
-            <Dialog.Content>
-              <Dialog.Title>Nova transação</Dialog.Title>
-
-              <Dialog.Close />
-            </Dialog.Content>
-          </Dialog.Portal>
+          <NewTransactionModal />
 
         </Dialog.Root>
       </HeaderContent>
